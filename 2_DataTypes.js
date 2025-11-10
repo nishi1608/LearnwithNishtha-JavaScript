b8629b962dad950c358b51dbe2a8dfbc53247460
Nishtha Jain <nishi1608@gmail.com>
2025-11-10 22:37:10 +0100

    Datatypes in JS added

---
+++ 2_DataTypes.js
@@ -0,0 +1,7 @@
+/*JavaScript has 8 main data types, divided into primitive and non-primitive categories. 
+These types define how values are stored, accessed, and manipulated in your code.*/
+
+//1.Number
+let num=1;
+console.log("num is"+num); //1
+console.log(typeof(num));  //number
